0a413c8dacee323f36398f2f5e7f3e8afc72ff00
yashtayalcse <yashtayal28763@gmail.com>
2025-06-30 20:36:53 +0530

    created user controller register

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,24 @@
 import {Router} from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
 
-router.route('/register').post(registerUser);
+router.route('/register').post(
+    // a middleware 
+    upload.fields([
+        {
+            name: "avatar",
+            maxCount: 1
+        },
+        {
+            name: "coverImage",
+            maxCount: 1
+        }
+    ]),
+
+    registerUser
+); 
+//register user is main task to be performed after middleware
 
 export default router;
